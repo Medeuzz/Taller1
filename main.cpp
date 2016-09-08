f75cbee4bea23b09c05b4893f2bfd7eb62577b2f
Medeuzz <meduzin04@gmail.com>
2016-09-08 00:05:21 -0500

    cambio

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int k;
+    cout << "Hello world!" << endl;
+    // eso es
+    //caqrajo
+    cin>>k;
+    cout<<"Hola guapo"<<endl;
+    return 0;
+}
